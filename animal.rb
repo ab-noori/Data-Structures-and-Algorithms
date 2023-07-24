de1e0b3a0c0fde461be01b8628fe09f3da27134c
Abdul Ali Noori <ab.noori.1993@gmail.com>
2023-07-25 01:14:09 +0330

    Update animal.rb

--- animal.rb
+++ animal.rb
@@ -5,4 +5,24 @@ class Animal
     @number_of_legs = number_of_legs
     @type = type
   end
+
+  def id
+    @id
+  end
+
+  def type
+    @type
+  end
+
+  def number_of_legs
+    @number_of_legs
+  end
+
+  def name
+    @name
+  end
+
+  def name=(value)
+    @name = value
+  end
 end
