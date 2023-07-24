f21775ef2b36343e93c55a93b026c737c43a71fd
Abdul Ali Noori <ab.noori.1993@gmail.com>
2023-07-25 01:02:14 +0330

    Create animal.rb

---
+++ animal.rb
@@ -0,0 +1,8 @@
+class Animal
+  def initialize(type, number_of_legs, name = "Unknown")
+    @id = Random.rand(1..1000)
+    @name = name
+    @number_of_legs = number_of_legs
+    @type = type
+  end
+end
