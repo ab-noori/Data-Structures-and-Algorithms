edd718478d04600a8033a409f3836acfb1019071
Abdul Ali Noori <ab.noori.1993@gmail.com>
2023-10-24 14:07:23 +0000

    Add method description with comment.

--- practice-js/linked-list/LinkedList.js
+++ practice-js/linked-list/LinkedList.js
@@ -3,4 +3,19 @@ class LinkedList {
         this.head = null;
         this.size = 0;
     }
+
+    // Insert firt node
+
+    // Insert last node
+
+    // Insert at index
+
+    // Get at index
+
+    // Remove at index
+
+    // Clear list
+
+    // Print list data
+    
 }
