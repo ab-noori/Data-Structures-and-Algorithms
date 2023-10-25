95e5b94c2a4dcc196bb1b0e7d28cfa2097458113
Abdul Ali Noori <ab.noori.1993@gmail.com>
2023-10-25 12:45:04 +0000

    Add the insertAt method to the linkedList.

--- practice-js/linked-list/LinkedList.js
+++ practice-js/linked-list/LinkedList.js
@@ -1,14 +1,74 @@
+class Node {
+    constructor(data, next = null){
+        this.data = data;
+        this.next = next;
+    }
+}
+
 class LinkedList {
-    constructor(){
+    constructor() {
         this.head = null;
         this.size = 0;
     }
 
     // Insert firt node
+    insertFirst(data){
+        this.head = new Node(data, this.head);
+        this.size++;
+    }
 
     // Insert last node
+    insertLast(data) {
+        let node = new Node(data);
+        let current;
+
+        // if empty, make head
+        if(!this.head){
+            this.head = node;
+        } else {
+            current = this.head;
+
+            while(current.next){
+                current = current.next;
+            }
+
+            current.next = node;
+        }
+
+        this.size++;
+    }
 
     // Insert at index
+    insertAt(data, index) {
+        if(index > 0 && index > this.size) {
+            return;
+        }
+
+        // if index is first index
+        if(index === 0){
+            this.head = new Node(data, this.head);
+            return;
+        }
+
+        const node = new Node(data);
+        let current, previous;
+
+        // set current to first
+        current = this.head;
+
+        let count = 0;
+
+        while(count < index) {
+            previous = current; // Node before the index
+            count++;
+            current = current.next; // Node after the index
+        }
+
+        node.next = current;
+        previous.next = node;
+
+        this.size++;
+    }
 
     // Get at index
 
@@ -16,6 +76,23 @@ class LinkedList {
 
     // Clear list
 
-    // Print list data
-    
+    // Print list data 
+    PrintListData() {
+        let current = this.head;
+
+        while(current) {
+            console.log(current.data)
+            current = current.next;
+        }
+    }
 }
+
+const ll = new LinkedList()
+
+ll.insertFirst(100);
+ll.insertFirst(200);
+ll.insertFirst(300);
+ll.insertLast(499);
+ll.insertAt(313, 4)
+
+ll.PrintListData();
