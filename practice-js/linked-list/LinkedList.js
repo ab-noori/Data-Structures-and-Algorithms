c9b20f1e954f211a1d34f8611aafec7654ec7d12
Abdul Ali Noori <ab.noori.1993@gmail.com>
2023-11-04 15:14:04 +0000

    Add the remove method.

--- practice-js/linked-list/LinkedList.js
+++ practice-js/linked-list/LinkedList.js
@@ -88,6 +88,29 @@ class LinkedList {
     }
 
     // Remove at index
+    removeAt(index) {
+        if(index < 0 || index > this.size) {
+            return;
+        }
+
+        let current = this.head;
+        let previous;
+        let count = 0;
+
+        if(index === 0) {
+            this.head = current.next;
+        } else {
+            while(count < index) {
+                count++;
+                previous = current;
+                current = current.next;
+            }
+
+            previous.next = current.next;
+        }
+
+        this.size--;
+    }
 
     // Clear list
 
@@ -110,4 +133,8 @@ ll.insertFirst(300);
 ll.insertLast(499);
 ll.insertAt(313, 4)
 
+ll.removeAt(1);
+
 ll.PrintListData();
+
+// ll.gitAt(4);
